a2834c4907c6ef24219633a9a0fabd7bce333934
Colin552 <colin-strong@hotmail.com>
2020-01-13 10:00:45 -0500

    Added queries for products

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,15 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var client = require('../client.js');
 
-/* Get all products. */
+/* get all products. */
 router.get('/', function (req, res, next) {
-    res.send('All Products');
+
+    let queryString =   "SELECT product.product_name, product.price, brand.brand_name \
+                        FROM product INNER JOIN brand ON product.brand_id=brand.id"
+
+
+    client.query(queryString,
+        (err, products) => {
+            if (err) {
+                console.log(err.stack);
+            }
+            else {
+                res.send(products.rows);
+            }
+        })
 });
 
-/* Get product by id. */
+/* send productId and return a single product */
 router.get('/:id', function (req, res, next) {
-    res.send('Get product by id');
+    let value = [req.params.id];
+    let queryString =   "SELECT product.product_name, product.price, brand.brand_name \
+                        FROM product INNER JOIN brand ON product.brand_id=brand.id \
+                        WHERE product.id = $1";
+    
+    client.query(queryString, value,
+        (err, products) => {
+            if (err) {
+                console.log(err.stack);
+            }
+            else {
+                res.send(products.rows);
+            }
+        })
+        
 });
+
+/* send a categoryId and get all products in a specific category */
+router.get('/category/:id', function (req, res, next) {
+    let value = [req.params.id];
+    let queryString =   "SELECT product_category.product_id, product.product_name, product.price, brand.brand_name \
+                        FROM product_category \
+                        INNER JOIN product \
+                        ON product_category.product_id=product.id \
+                        INNER JOIN brand \
+                        ON product.brand_id=brand.id \
+                        WHERE category_id = $1"
+    
+    client.query(queryString, value,
+        (err, products) => {
+            if (err) {
+                console.log(err.stack);
+            }
+            else {
+                res.send(products.rows);
+            }
+        })
+        
+});
+
 
 /* Create a product. */
 router.post('/', function (req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,33 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var client = require('../client.js');
 
 /* Get users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Get all users');
+router.get('/', function (req, res, next) {
+  let queryString = "SELECT * from user_account"
+  client.query(queryString,
+    (err, users) => {
+      if (err) {
+        console.log(err.stack);
+      }
+      else {
+        res.send(users.rows);
+      }
+    })
 });
 
 /* Get user by ID. */
-router.get('/:userId', function(req, res, next) {
-  res.send('Get user by ID');
+router.get('/:userId', function (req, res, next) {
+  let value = [req.params.id];
+  let queryString = "SELECT * from user_account \
+                      WHERE product.id = $1";
+
+  client.query(queryString, value,
+    (err, users) => {
+      if (err) {
+        console.log(err.stack);
+      }
+      else {
+        res.send(users.rows);
+      }
+    })
 });
 
 /* Get user by email. */
-router.get('/email/:email', function(req, res, next) {
+router.get('/email/:email', function (req, res, next) {
   res.send('Get user by email');
 });
 
 /* Create a new user. */
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
   res.send('Create user');
 });
 
 /* Update a user. */
-router.patch('/', function(req, res, next){
+router.patch('/', function (req, res, next) {
   res.send('Update user');
 });
 
 /* Update a user. */
-router.delete('/', function(req, res, next){
+router.delete('/', function (req, res, next) {
   res.send('Delete user');
 });
 
